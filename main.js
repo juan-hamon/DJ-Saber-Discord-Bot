edcecdf3a6a58ae86e6440cdd06d4b171d0758c0
juan-hamon <53021906+juan-hamon@users.noreply.github.com>
2021-12-05 21:11:27 -0500

    Corrección de bug de no verificación de prefijo para comandos

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { client } = require("./bot.js")
-const { token, prefix } = require("./config.js")
+const { client } = require("./bot.js");
+const { token, prefix } = require("./config.js");
+const { createCommandDoesNotExistEmbed } = require("./embeds/errors/commandDoesNotExist.js");
 const fs = require('fs');
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -15,7 +16,7 @@ client.login(token);
 
 client.on("ready", ()=>{
     console.log(`${client.user.tag} Is online`)
-})
+});
 
 client.on("messageCreate", async (message) =>{
     if(!message.guild){
@@ -24,10 +25,16 @@ client.on("messageCreate", async (message) =>{
     if(message.author.bot){
         return;
     }
+    if(!message.content.includes(prefix)){
+        return;
+    }
     const args = message.content.slice(prefix.length).split(" ");
     const command = args.shift();
     const func = commands.get(command);
     if(func != undefined){
         await func(message, args);
+    }else{
+        let notFoundCommandEmbed = createCommandDoesNotExistEmbed(command.toUpperCase());
+        await message.channel.send({ embeds: [notFoundCommandEmbed] });
     }
-})
+});
